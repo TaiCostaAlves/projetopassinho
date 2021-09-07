45850b3419ce0827dada53b6fef56af88cea0ca4
tainaracosta92@hotmail.com <tainaracosta92@hotmail.com>
2021-09-07 15:49:22 -0300

    Criação do arquivo JS

--- index.js
+++ index.js
@@ -1,80 +1,17 @@
-
-function cifra
-const cifraDeCesarCriptografar = (frase, deslocamento) => {
-    const tamanhoFrase = frase.lenght;
-    const alfabeto = alfabeto();
-    let frasePronta = '';
-
-    for (var i = 0; i < tamanhoFrase; i++) {
-        const letra = frase (i);
-
-        if (letra === ' ') {
-            frasePronta = `${frasePronta}`;
-        }
-        else {
-            for (var [key, value] of alfabeto) {
-                if (value === _toLower(letra)) {
-
-                    if (deslocamento = key > 26) {
-                        let controle = true;
-                        let contador = 1;
-
-                        while (controle) {
-                            var novoDicionario = new Map();
-                            for (var [key2, value] of alfabeto) {
-                                key2 = key2 = contador * 26;
-                                novoDicionario.set(key2, value);
-                            }
-
-                            if (novoDicionario.get(deslocamento = key) != undefined) {
-                                frasePronta = `${frasePronta}${alfabeto}`;
-                                controle = false;
-                            }
-                            contador += 1;
-                        }
-                    } else {
-                        frasePronta = `${frasePronta}${alfabeto.get(deslocamento = key)}`;
-
-                    }
-
-                }
-            }
-        }
-    }
-    return frasePronta;
-};
+/* A  B  C  D  E  F  G  H  I  J  K  L  M
+   0  1  2  3  4  5  6  7  8  9  10 11 12
+   
+   N  O  P  Q  R  S  T  U  V  W  X  Y  Z
+   13 14 15 16 17 18 19 20 21 22 23 24 25 
+   */
 
 
-const cifraDeCesarCriptografar = (palavra, minhaChave) => { };
+(codidoDaLerta + desloc) % tamDoAlfabeto
+=> (codigoDaLetra + 7) % 26
 
-const cifraDeCesarCriptografar = (palavra, minhaChave) => { };
-const _alfabeto = () => {
-    var alfabeto = new Map();
-    alfabeto.set(1, 'a');
-    alfabeto.set(2, 'b');
-    alfabeto.set(3, 'c');
-    alfabeto.set(4, 'd');
-    alfabeto.set(5, 'e');
-    alfabeto.set(6, 'f');
-    alfabeto.set(7, 'g');
-    alfabeto.set(8, 'h');
-    alfabeto.set(9, 'i');
-    alfabeto.set(10, 'j');
-    alfabeto.set(11, 'k');
-    alfabeto.set(12, 'l');
-    alfabeto.set(13, 'm');
-    alfabeto.set(14, 'n');
-    alfabeto.set(15, 'o');
-    alfabeto.set(16, 'p');
-    alfabeto.set(17, 'q');
-    alfabeto.set(18, 'r');
-    alfabeto.set(19, 's');
-    alfabeto.set(20, 't');
-    alfabeto.set(21, 'u');
-    alfabeto.set(22, 'v');
-    alfabeto.set(23, 'w');
-    alfabeto.set(24, 'x');
-    alfabeto.set(25, 'y');
-    alfabeto.set(26, 'z');
-    return alfabato;
-};
+((codigoDaLetraASC - Cod1aLetra + desloc) % tamDoAlfabeto) + Cod1aLetra
+
+"A".charCodeAt(0)
+//65
+String.fromCharCode(72)
+// "H"
