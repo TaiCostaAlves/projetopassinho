61d77b248b85eaf2f2eb6353a4c2dbd3e9226f97
tainaracosta92@hotmail.com <tainaracosta92@hotmail.com>
2021-09-08 17:46:21 -0300

    Adicionado arquivo js final

--- index.js
+++ index.js
@@ -1,17 +1,83 @@
-/* A  B  C  D  E  F  G  H  I  J  K  L  M
-   0  1  2  3  4  5  6  7  8  9  10 11 12
-   
-   N  O  P  Q  R  S  T  U  V  W  X  Y  Z
-   13 14 15 16 17 18 19 20 21 22 23 24 25 
-   */
+var texto = document.getElementById ("digiteasuamensagem")
+var caixa = document.getElementById ("caixa")
+var escolha = document.getElementById ("escolha")
+var deslocamento = document.getElementById ("numerodeslocamento")
+var gerarcodif = document.getElementById ("botao")
+var resultado = ""
+var mensagemfinal = document.getElementById ("mensagemfinal")
+var botaocodif = document.getElementById ("radio-codificar")
+botaocodif.addEventListener("click", function () {
+    gerarcodif.textContent="codificar mensagem"
+} )
+var botaodecodif = document.getElementById ("radio-decodificar")
+botaodecodif.addEventListener("click", function () {
+    gerarcodif.textContent="codificar mensagem"
+} )
+document.getElementById("botao").onclick= escolhaopcao;
+function escolhaopcao (event){
+    event.preventDefault ()
+    var valorescolhido = escolha.value 
+        if (valorescolhido=="cifra") {
+                if (gerarcodif.textContent=="codificar mensagem"){
+                    resultado = cifra(texto.value,deslocamento.value)
+                    mensagemfinal.textContent=resultado
+                }else {
+                    resultado = descodificacifra(texto.value,deslocamento.value)
+                    mensagemfinal.textContent=resultado
+                }
+        } else {
+            if (gerarcodif.textContent=="codificar mensagem"){
+                resultado = window.btoa(texto.value)
+                mensagemfinal.textContent=resultado
+            }else {
+                resultado = window.atob(texto.value)
+                mensagemfinal.textContent=resultado
+            }
+        }
+        console.log(resultado)
+}
+function cifra(mensagem, deslocamento) {
+    mensagem = mensagem.toLowerCase
+    var mensagemcodificada =[] 
+    var mensagemfinal = []
+    var desloc = parseInt(deslocamento)
+    for (var i=0; i<mensagem.length; i++) {
+        if (mensagem[i]== " ") {
+             mensagemcodificada.push(mensagem[i].charCodeAt)
+        }else {
+            mensagemcodificada.push((((mensagem[i].charCodeAt) + desloc - 97)%26)+97)
+        }
+    }
+    for (var j=0; j<mensagemcodificada.length; j++){
+        mensagemfinal.push(String.fromCharCode(mensagemcodificada[j]))
+    }
+    return mensagemfinal.join("")
+}
 
-
-(codidoDaLerta + desloc) % tamDoAlfabeto
-=> (codigoDaLetra + 7) % 26
-
-((codigoDaLetraASC - Cod1aLetra + desloc) % tamDoAlfabeto) + Cod1aLetra
-
-"A".charCodeAt(0)
-//65
-String.fromCharCode(72)
-// "H"
+function descodificacifra(mensagem, deslocamento) {
+   mensagem = mensagem.toLowerCase 
+    mensagem = mensagem.split("")
+    var mensagemcodificada =[] 
+    var mensagemfinal = []
+    var desloc = parseInt(deslocamento)
+    for (var i=0; i<mensagem.length; i++) {
+        if (mensagem[i]== " ") {
+             mensagemcodificada.push(mensagem[i].charCodeAt)
+        }else {
+            mensagemcodificada.push((((mensagem[i].charCodeAt) - desloc - 97)%26)+97)
+        }
+    }
+    for (var j=0; j<mensagemcodificada.length; j++){
+        mensagemfinal.push(String.fromCharCode(mensagemcodificada[j]))
+    }
+    return mensagemfinal.join("")
+}
+escolha.addEventListener("click",function desaparece(event){
+    var valorselecionado=escolha.value 
+    event.preventDefault ()
+    if (valorselecionado=="base"){
+        return caixa.style.display="none"
+    } else {
+        return caixa.style.disply = "block"
+    }
+})
