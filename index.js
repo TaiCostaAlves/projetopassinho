7d866fb923e252ca452db1fd9f092034c36d42c9
tainaracosta92@hotmail.com <tainaracosta92@hotmail.com>
2021-09-28 10:25:36 -0300

    Iniciando a estilização da page

--- index.js
+++ index.js
@@ -5,47 +5,53 @@ var deslocamento = document.getElementById ("numerodeslocamento")
 var gerarcodif = document.getElementById ("botao")
 var resultado = ""
 var mensagemfinal = document.getElementById ("mensagemfinal")
-var botaocodif = document.getElementById ("radio-codificar")
+var botaocodif = document.getElementById ("radioCodificar")
 botaocodif.addEventListener("click", function () {
     gerarcodif.textContent="codificar mensagem"
 } )
-var botaodecodif = document.getElementById ("radio-decodificar")
+var botaodecodif = document.getElementById ("radioDecodificar")
 botaodecodif.addEventListener("click", function () {
-    gerarcodif.textContent="codificar mensagem"
+    gerarcodif.textContent="decodificar mensagem"
 } )
 document.getElementById("botao").onclick= escolhaopcao;
 function escolhaopcao (event){
     event.preventDefault ()
     var valorescolhido = escolha.value 
+    
         if (valorescolhido=="cifra") {
-                if (gerarcodif.textContent=="codificar mensagem"){
+                if (botaocodif.checked){
                     resultado = cifra(texto.value,deslocamento.value)
-                    mensagemfinal.textContent=resultado
-                }else {
+                    mensagemfinal.value=resultado
+                    
+                }else if(botaodecodif.checked) {
                     resultado = descodificacifra(texto.value,deslocamento.value)
-                    mensagemfinal.textContent=resultado
+                    mensagemfinal.value=resultado
                 }
         } else {
-            if (gerarcodif.textContent=="codificar mensagem"){
+            if (botaocodif.checked){
                 resultado = window.btoa(texto.value)
-                mensagemfinal.textContent=resultado
-            }else {
+                mensagemfinal.value=resultado
+            }else if(botaodecodif.checked) {
                 resultado = window.atob(texto.value)
-                mensagemfinal.textContent=resultado
+                mensagemfinal.value=resultado
             }
         }
-        console.log(resultado)
+        
 }
 function cifra(mensagem, deslocamento) {
-    mensagem = mensagem.toLowerCase
+    mensagem = mensagem.toLowerCase()
+    mensagem = mensagem.split("")
     var mensagemcodificada =[] 
     var mensagemfinal = []
     var desloc = parseInt(deslocamento)
+    console.log(mensagem)
     for (var i=0; i<mensagem.length; i++) {
-        if (mensagem[i]== " ") {
-             mensagemcodificada.push(mensagem[i].charCodeAt)
+        var msg = mensagem[i].charCodeAt()
+        if (mensagem[i]!= 32) {
+            mensagemcodificada.push(((msg + desloc - 97)%26)+97)
+            console.log(msg)
         }else {
-            mensagemcodificada.push((((mensagem[i].charCodeAt) + desloc - 97)%26)+97)
+            mensagemcodificada.push(mensagem[i].charCodeAt)
         }
     }
     for (var j=0; j<mensagemcodificada.length; j++){
@@ -55,17 +61,20 @@ function cifra(mensagem, deslocamento) {
 }
 
 function descodificacifra(mensagem, deslocamento) {
-   mensagem = mensagem.toLowerCase 
+   mensagem = mensagem.toLowerCase()
     mensagem = mensagem.split("")
     var mensagemcodificada =[] 
     var mensagemfinal = []
-    var desloc = parseInt(deslocamento)
+   var desloc = parseInt(deslocamento)
     for (var i=0; i<mensagem.length; i++) {
         if (mensagem[i]== " ") {
-             mensagemcodificada.push(mensagem[i].charCodeAt)
-        }else {
-            mensagemcodificada.push((((mensagem[i].charCodeAt) - desloc - 97)%26)+97)
+             mensagemcodificada.push(mensagem[i].charCodeAt())
+        }else if (((mensagem[i].charCodeAt())-97-desloc)>=0) { 
+            mensagemcodificada.push((((mensagem[i].charCodeAt()) - desloc - 97)%26)+97)
+        } else {
+            mensagemcodificada.push((((mensagem[i].charCodeAt()) - desloc - 97)+26)+97)
         }
+
     }
     for (var j=0; j<mensagemcodificada.length; j++){
         mensagemfinal.push(String.fromCharCode(mensagemcodificada[j]))
